0450f90247b54db69ada7eeedaa7a9ddea351c02
Nimzyow <n_soufiani@hotmail.com>
2020-02-28 13:36:39 +0000

    created Congrats test and congrats component and tests pass

---
+++ src/Congrats.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+/**
+ * Functional react component for congratulary message
+ * @function
+ * @param {object} props - React props
+ * @returns {JSX.Element}
+ */
+export default props => {
+  if (props.success) {
+    return (
+      <div data-test="component-congrats">
+        <span data-test="congrats-message">Congratulations!</span>
+      </div>
+    );
+  } else {
+    return <div data-test="component-congrats"></div>;
+  }
+};
