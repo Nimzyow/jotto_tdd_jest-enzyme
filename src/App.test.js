93e930e06d114f3817940a145e62152e329c087c
Nimzyow <n_soufiani@hotmail.com>
2020-03-06 14:47:48 +0000

    convert  to hooks and context

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,18 @@
 import React from "react";
+import { shallow } from "enzyme";
+import { findByTestAttr } from "../test/TestUtils";
+import App from "./App";
 
-test("renders learn react link", () => {});
+/**
+ * setup function for app component
+ * @returns {ShallowWrapper}
+ */
+const setup = () => {
+  return shallow(<App />);
+};
+
+test("renders learn react link", () => {
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, "component-app");
+  expect(component.length).toBe(1);
+});
