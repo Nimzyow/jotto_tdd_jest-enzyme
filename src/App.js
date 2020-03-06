291f5c4b5087f0b3da42a0d7d0e135f64825557b
Nimzyow <n_soufiani@hotmail.com>
2020-03-06 15:37:46 +0000

    tested for input proptypes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
 import "./App.css";
-import GuessedWords from "./GuessedWords";
-import Congrats from "./Congrats";
 class App extends Component {
   render() {
     return <div data-test="component-app"></div>;
--- src/Input.js
+++ src/Input.js
@@ -1,7 +1,11 @@
 import React from "react";
-
-const Input = () => {
+import PropTypes from "prop-types";
+const Input = ({ secretWord }) => {
   return <div data-test="component-input"></div>;
+};
+
+Input.propTypes = {
+  secretWord: PropTypes.string.isRequired
 };
 
 export default Input;
