c31494ff03f6cbd16e2125ea2c0ca54677995ba8
Nimzyow <n_soufiani@hotmail.com>
2020-03-13 16:57:44 +0000

    testing with moxios

--- src/Input.test.js
+++ src/Input.test.js
@@ -23,3 +23,34 @@ test("does not throw warning with expected props", () => {
   const expectedProps = defaultProps;
   checkProps(Input, expectedProps);
 });
+
+describe("state controlled input field", () => {
+  let mockSetCurrentGuess = jest.fn();
+  let wrapper;
+  beforeEach(() => {
+    //below will clear the mockSetCurrentGuess double so that it doesn't carry on the results to the next tests
+    mockSetCurrentGuess.mockClear();
+    //we grab the useState and set the mock below
+    React.useState = jest.fn(() => ["", mockSetCurrentGuess]);
+    wrapper = setup();
+  });
+
+  test("state updates with value of input box upon change", () => {
+    //jest.fn just sets it up so that its a mock
+
+    const inputBox = findByTestAttr(wrapper, "input-box");
+
+    //mock event
+    // we set the target.value to be train here. and assign it to mockEvent
+    const mockEvent = { target: { value: "train" } };
+    //when we click on input we pass mock Event
+    inputBox.simulate("change", mockEvent);
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith("train");
+  });
+  test("field is cleared upon submit button click", () => {
+    const submitButton = findByTestAttr(wrapper, "submit-button");
+
+    submitButton.simulate("click", { preventDefault() {} });
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith("");
+  });
+});
